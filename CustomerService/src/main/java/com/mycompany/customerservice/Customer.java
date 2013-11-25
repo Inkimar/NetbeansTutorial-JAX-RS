e6e5c9873ca99028fc1df4dde78f3c46c3967b16
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-11-26 00:38:28 +0100

    moving to the fields

--- CustomerService/src/main/java/com/mycompany/customerservice/Customer.java
+++ CustomerService/src/main/java/com/mycompany/customerservice/Customer.java
@@ -19,6 +19,10 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -28,6 +32,7 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @Table(name = "CUSTOMER")
+@XmlAccessorType(value = XmlAccessType.FIELD)
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c"),
@@ -42,47 +47,63 @@ import javax.xml.bind.annotation.XmlTransient;
     @NamedQuery(name = "Customer.findByEmail", query = "SELECT c FROM Customer c WHERE c.email = :email"),
     @NamedQuery(name = "Customer.findByCreditLimit", query = "SELECT c FROM Customer c WHERE c.creditLimit = :creditLimit")})
 public class Customer implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     @Id
     @Basic(optional = false)
     @NotNull
     @Column(name = "CUSTOMER_ID")
     private Integer customerId;
+
     @Size(max = 30)
     @Column(name = "NAME")
     private String name;
+
     @Size(max = 30)
     @Column(name = "ADDRESSLINE1")
     private String addressline1;
+
     @Size(max = 30)
     @Column(name = "ADDRESSLINE2")
     private String addressline2;
+
     @Size(max = 25)
     @Column(name = "CITY")
     private String city;
+
     @Size(max = 2)
     @Column(name = "STATE")
     private String state;
     // @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$", message="Invalid phone/fax format, should be as xxx-xxx-xxxx")//if the field contains phone or fax number consider using this annotation to enforce field validation
+
     @Size(max = 12)
     @Column(name = "PHONE")
     private String phone;
     // @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$", message="Invalid phone/fax format, should be as xxx-xxx-xxxx")//if the field contains phone or fax number consider using this annotation to enforce field validation
+
     @Size(max = 12)
     @Column(name = "FAX")
     private String fax;
     // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+
     @Size(max = 40)
     @Column(name = "EMAIL")
     private String email;
+
     @Column(name = "CREDIT_LIMIT")
     private Integer creditLimit;
+
     @JoinColumn(name = "ZIP", referencedColumnName = "ZIP_CODE")
     @ManyToOne(optional = false)
     private MicroMarket zip;
+
     @JoinColumn(name = "DISCOUNT_CODE", referencedColumnName = "DISCOUNT_CODE")
     @ManyToOne(optional = false)
     private DiscountCode discountCode;
+
+  
+    @XmlTransient
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "customerId")
     private Collection<PurchaseOrder> purchaseOrderCollection;
 
@@ -189,7 +210,7 @@ public class Customer implements Serializable {
         this.discountCode = discountCode;
     }
 
-    @XmlTransient
+    // @XmlTransient
     public Collection<PurchaseOrder> getPurchaseOrderCollection() {
         return purchaseOrderCollection;
     }
@@ -222,5 +243,5 @@ public class Customer implements Serializable {
     public String toString() {
         return "com.mycompany.customerservice.Customer[ customerId=" + customerId + " ]";
     }
-    
+
 }
