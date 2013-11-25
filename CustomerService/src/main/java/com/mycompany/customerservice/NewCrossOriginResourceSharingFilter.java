9dd09eeb7340c493322f9709a2b9baf0aefa3674
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-11-26 00:16:43 +0100

    First checking. Using database 'SAMPLE' - found in Netbeans Plugins

---
+++ CustomerService/src/main/java/com/mycompany/customerservice/NewCrossOriginResourceSharingFilter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.customerservice;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+/**
+ *
+ * @author Geertjan
+ */
+public class NewCrossOriginResourceSharingFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "content-type");
+        return response;
+    }
+    
+}
---
+++ CustomerService/src/main/java/service/CustomerFacadeREST.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import com.mycompany.customerservice.Customer;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author Geertjan
+ */
+@Stateless
+@Path("com.mycompany.customerservice.customer")
+public class CustomerFacadeREST extends AbstractFacade<Customer> {
+    @PersistenceContext(unitName = "com.mycompany_CustomerService_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    public CustomerFacadeREST() {
+        super(Customer.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Customer entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void edit(Customer entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Integer id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Customer find(@PathParam("id") Integer id) {
+        Customer customer = super.find(id);
+        return customer;
+    }
+
+    @GET
+    @Override
+    @Produces({"application/json"})
+    public List<Customer> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Customer> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
